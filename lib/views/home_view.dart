ff0bdb056d19444619b81e8fda491e9b14bc074d
BIJOY-PAUL <bijoyrudrapaul78@gmail.com>
2025-12-25 10:51:44 +0600

    add new clean code

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -5,6 +5,23 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Learn',
+
+      home: Container(
+        color: Colors.deepOrange,
+        child: const Center(
+          child: Text(
+            "Hello World",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 40,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
